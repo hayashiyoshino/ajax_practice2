3a5e01ae5ca0c32fa6ce1017ae13c4fc54fc718b
林佳志乃 <twinklestaryoshino@gmail.com>
2018-09-27 19:37:34 +0900

    done

--- script.js
+++ script.js
@@ -4,13 +4,18 @@ const API_KEY = 'd331f2554c1fc49908a3d0547c063624'
 $(function() {
   $('#weather-form').on('submit', function(e) {
     var cityName = $(this).find('#weather-form-city').prop('value');
-
-    $.ajax('http://api.openweathermap.org/data/2.5/weather_APPID=' + API_KEY + '&q' + cityName);
+    $.ajax('http://api.openweathermap.org/data/2.5/weather?APPID=' + API_KEY + '&q=' + cityName)
       .done(function(data) {
-
+        $('#form-error').css('display', 'none');
+        $('#result-city-name').text(data.name);
+        $('#result-temp').text(Math.round(data.main.temp - 273));
+        $('#result-weather').text(data.weather[0].main);
+        $('#result-datetime').text(Date(data.dt));
+        $('#form-result').css('display', 'block');
       })
       .fail(function(data) {
-
+        $('#form-spinner').css('display', 'none');
+        alert('Something wrong occurred.');
       });
     e.preventDefault();
   });
